59f77ef7d6e1d3b1419ccdba2424606e9ef2c547
Majid Arthaud <arthaud.majid@gmail.com>
2024-07-30 16:13:09 +0200

    Reformatting projection

--- Finite_difference_Laville_Antonin.py
+++ Finite_difference_Laville_Antonin.py
@@ -2,23 +2,27 @@ import numpy as np
 import scipy.fftpack as sft
 import matplotlib.pyplot as plt
 
-## this projects onto Free Divergence fields
-def ProjDiv(z): # z = array[M,N,2]
-    # the last line/column of zx, zy have to be zero
-    [M,N,D] = z.shape
-    # z[-1,:,0]=0
-    # z[:,-1,1]=0
+# Getting image potential
+g = plt.imread("virgule.png")
+sh = g.shape
+if len(sh) == 3:
+    g = g[:,:,0:3].mean(axis=2)
+M = sh[0]
+N = sh[1]
 
-    # the filter could be built separately beforehand
-    x = np.reshape(np.arange(N),(1,N))
-    y = np.reshape(np.arange(M),(M,1))
-    x = np.ones((M,1))*x;
-    y = y*np.ones((1,N));
-    Filt = 2*((np.cos(np.pi*x/N)-1) + (np.cos(np.pi*y/M)-1))
-    Filt = 1./(5e-14-Filt)
-    Filt[0,0]=0
-    # end of filter
-    
+weights = 1/(.1+g)
+
+# Defining filter for inverse Neumann Laplacian
+x = np.reshape(np.arange(N),(1,N))
+y = np.reshape(np.arange(M),(M,1))
+x = np.ones((M,1))*x
+y = y*np.ones((1,N))
+Filt = 2*((np.cos(np.pi*x/N)-1) + (np.cos(np.pi*y/M)-1))
+Filt = 1./(5e-14-Filt)
+Filt[0,0]=0
+
+# This projects onto Free Divergence fields
+def ProjDiv(z):
     u = np.vstack([z[0,:,0], z[1:-1,:,0]-z[0:-2,:,0], -z[-2,:,0]]) + np.hstack([np.reshape(z[:,0,1],(M,1)), z[:,1:-1,1]-z[:,0:-2,1], np.reshape(-z[:,-2,1], (M,1))])
     u = -sft.dctn(sft.dctn(u,type=2)*Filt,type=3)/(4*M*N) # -(D^*D)^{-1} [-D^* z]
 
@@ -28,24 +32,6 @@ def ProjDiv(z): # z = array[M,N,2]
     z[:,:,1] = z[:,:,1]-duy
     return z
 
-g = plt.imread("virgule.png")
-sh = g.shape
-if len(sh) == 3:
-    g = g[:,:,0:3].mean(axis=2)
-M = sh[0]
-N = sh[1]
-
-# the filter could be built separately beforehand
-x = np.reshape(np.arange(N),(1,N))
-y = np.reshape(np.arange(M),(M,1))
-x = np.ones((M,1))*x
-y = y*np.ones((1,N))
-Filt = 2*((np.cos(np.pi*x/N)-1) + (np.cos(np.pi*y/M)-1))
-Filt = 1./(5e-14-Filt)
-Filt[0,0]=0
-# end of filter
-
-weights = 1/(.1+g) # +x*x+y*y does not work # find some image
 mu = np.zeros((M,N))
 mu[M//20,5*N//10]=1
 mu[4*M//10,19*N//20]=-1
